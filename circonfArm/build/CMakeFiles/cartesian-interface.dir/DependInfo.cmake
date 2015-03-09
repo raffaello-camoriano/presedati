4f67710d4eac2294a5e57e846b00e1db28cb7da5
Raffaello Camoriano <raffaello.camoriano@iit.it>
2015-03-09 10:48:31 +0100

    1st commit

---
+++ circonfArm/build/CMakeFiles/cartesian-interface.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kammo/Repos/presedati/circonfArm/src/main.cpp" "/home/kammo/Repos/presedati/circonfArm/build/CMakeFiles/cartesian-interface.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/opencv"
+  "/home/kammo/Repos/yarp/build/generated_include"
+  "/home/kammo/Repos/yarp/src/libYARP_OS/include"
+  "/home/kammo/Repos/yarp/src/libYARP_sig/include"
+  "/home/kammo/Repos/yarp/src/libYARP_math/include"
+  "/home/kammo/Repos/yarp/src/libYARP_dev/include"
+  "/home/kammo/Repos/yarp/src/libYARP_name/include"
+  "/home/kammo/Repos/yarp/src/libYARP_manager/include"
+  "/home/kammo/Repos/yarp/src/libYARP_logger/include"
+  "/home/kammo/Repos/yarp/src/yarpserver/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
